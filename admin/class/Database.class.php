42cef34c21c14c110f01ffc382f90f5d490aeaf8
Majidli Nijat <nijatmajidli@gmail.com>
2023-02-21 19:11:57 +0100

    modif

--- admin/class/Database.class.php
+++ admin/class/Database.class.php
@@ -12,6 +12,12 @@ class Database extends Upload
     public static $select="*";
     public static $whereRawKey;
     public static $whereRawValue;
+    public static $whereKey;
+    public static $whereValue=array();
+    public static $orderBy=null;
+    public static $limit=null;
+    public static $join="";
+    public static $leftJoin="";
 
 
     // Constructor method calls __connect() method
@@ -21,15 +27,17 @@ class Database extends Upload
     // Creates database connection
     public static function __connect()
     {
-        try{
+        try
+        {
             // $db = new PDO("mysql:host=localhost;dbname=adminpanel;charset=utf8", "root", "");
             self::$connection = new PDO("mysql:host=".self::HOST.";dbname=".self::DATABASE.";charset=utf8", self::USERNAME, self::PASSWORD);
             
             return "Database connection successfully established !";
         } 
-        catch(PDOException $error){
-            // By using (Object) syntaxe we convert array $data into an object 
-            $data =(Object) [
+        catch(PDOException $error)
+        {
+            // (Object) converts array $data into an object 
+            $data = (Object) [
                 "title"=>"Connection Error",
                 "code"=>$error->getCode(),
                 "description"=> "Database connection failed.",  
@@ -63,8 +71,15 @@ class Database extends Upload
     {
         self::$table = $tableName;
         self::$select = "*";
-        self::$whereRawKey = null;
-        self::$whereRawValue = null;
+        self::$whereRawKey=null;
+        self::$whereRawValue=null;
+        self::$whereKey=null;
+        self::$whereValue=array();
+        self::$orderBy=null;
+        self::$limit=null;
+        self::$join="";
+        self::$leftJoin="";
+
         return new self;    // self keyword refers to Database class
     }
 
@@ -86,6 +101,129 @@ class Database extends Upload
     }
 
     
+    // Returns a new Database class with public static $whereKey = $param and $whereValue = $param2 ou $param3
+    public static function where($param1, $param2=null, $param3=null)
+    {
+        if(is_array($param1))
+        {
+            $keyList = array();
+
+            foreach($param1 as $key=>$value)
+            {
+                $keyList[] = $key;
+                self::$whereValue[] = $value;   
+            }
+
+            self::$whereKey = implode("=? AND ", $keyList)."=?";
+        }
+        else if($param2!=null && $param3==null)
+        {
+            self::$whereValue[] = $param2;
+            self::$whereKey = $param1."=?";
+        }
+        else if($param3!=null)
+        {
+            self::$whereValue[] = $param3;
+            self::$whereKey = $param1.$param2."?"; 
+        }
+        
+        return new self;
+    }
+
+
+    // 
+    public static function orderBy($parameter)
+    {
+        self::$orderBy = $parameter[0]." ".(!empty($parameter[1]) ? $parameter[1] : "ASC");
+        return new self;
+    }
+
+
+    // 
+    public static function limit($start, $end=null)
+    {
+        self::$limit = $start.($end!=null ? ",".$end : "");
+        return new self;
+    }
+
+
+    public static function join($tableName, $thisColumn, $joinColumn)
+    {
+        self::$join.="INNER JOIN ".$tableName." ON ".self::$table.".".$thisColumn."=".$tableName.".".$joinColumn." "; 
+        return new self;
+    }
+
+
+    public static function leftJoin($tableName, $thisColumn, $joinColumn)
+    {
+        self::$leftJoin.="LEFT JOIN ".$tableName." ON ".self::$table.".".$thisColumn."=".$tableName.".".$joinColumn." "; 
+        return new self;
+    }
+
+
+    public static function get()
+    {
+        $SQL = "SELECT ".self::$select." FROM ".self::$table." ";
+        $SQL.= !empty(self::$join) ? self::$join : " ";
+        $SQL.= !empty(self::$leftJoin) ? self::$leftJoin : " ";
+        
+        $WHERE = null;
+
+        if(!empty(self::$whereKey) && !empty(self::$whereRawKey))
+        {
+            $SQL.= "WHERE ".self::$whereKey." AND ".self::$whereRawKey." ";
+            $WHERE = array_merge(self::$whereValue, self::$whereRawValue);
+        }
+        else
+        {
+            if(!empty(self::$whereKey))
+            {
+                $SQL.= "WHERE ".self::$whereKey." ";
+                $WHERE = self::$whereValue;
+            }   
+            if(!empty(self::$whereRawKey))
+            {
+                $SQL.= "WHERE ".self::$whereRawKey." ";
+                $WHERE = self::$whereRawValue;
+            }
+        }
+
+        $SQL.= !empty(self::$orderBy) ? "ORDER BY ".self::$orderBy." " : "";
+        $SQL.= !empty(self::$limit) ? "LIMIT ".self::$limit : "";
+
+        if($WHERE!=null)
+        {
+            $Entity = self::$connection->prepare($SQL);
+            $Entity->execute($WHERE);
+        }
+        else
+        {
+            $Entity = self::$connection->query($SQL);
+        }
+        
+        $Result = $Entity->fetchAll(PDO::FETCH_ASSOC);
+        
+        if($Result)
+        {
+            $data = [];
+            
+            foreach($Result as $item)
+            {
+                $data[] = (object) $item;
+            }
+
+            return $data;
+        }
+        else
+        {
+            return false;
+        }
+
+        
+    }
+
+
+
 
 }
 
--- admin/index.php
+++ admin/index.php
@@ -12,12 +12,48 @@ include_once("class/Database.class.php");
 // Database::table("corporate")->select("title, description, id");
 // Database::table("corporate")->select(["title, description", "id"]);
 
+// SELECT * FROM corporate WHERE title=? AND id=? AND description=?
+// Database::table("corporate")->where(["title"=>"bilgi", "id"=>3, "description"=>"aciklama"]);
+// echo Database::$whereKey;
+
+// SELECT * FROM corporate WHERE id=3
+// Database::table("corporate")->where("id", 3);
+// echo Database::$whereKey;
+
+// SELECT * FROM corporate WHERE id > 3
+// Database::table("corporate")->where("id", '>', 3);
+// echo Database::$whereKey;
+
+// SELECT * FROM corporate WHERE title LIKE %a% ORDER BY id DESC LIMIT 1,5
+// Database::table("corporate")->where("title", " LIKE ", "%a%")->orderBy(["id", "DESC"])->limit(1,5);
+// echo Database::$orderBy;
+// echo Database::$limit;
+
+// SELECT * FROM corporate INNER JOIN categories ON corporate.category=categories.ID;
+// Database::table("corporate")->join("categories", "category", "ID");
+// echo Database::$join;
 
 
-// SELECT * FROM "corporate" WHERE title LIKE "%a%" AND description="bilgi"
-Database::table("corporate")
-->select(["id", "title", "description"])
-->whereRaw("title LIKE '%?%' AND description=?", ["a", "bilgi"]);
+// SELECT corporate.id, corporate.title, corporate.description, categories.title AS categoryName 
+// FROM corporate INNER JOIN categories ON corporate.category=categories.ID WHERE corporate.id=? 
+// AND corporate.title LIKE "%a%" ORDER BY corporate.title DESC LIMIT 0,10
+$result = Database::table('corporate')
+            ->select(["corporate.id", "corporate.title", "corporate.description", "categories.title AS categoryName"])
+            ->join("categories", "category", "ID")
+            // ->leftJoin("brands", "brandID", "ID")
+            // ->where("corporate.id", 1)
+            ->whereRaw("corporate.description LIKE ?", ["%a%"])
+            ->orderBy(['corporate.title', 'DESC'])
+            ->limit(0,10)
+            ->get();
+
+if($result)
+{
+    foreach($result as $item)
+    {
+        echo $item->title."---".$item->categoryName."<br>";
+    }
+}
 
 
 ?>
